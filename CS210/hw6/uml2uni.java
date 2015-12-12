4b9f574edc3596183e643f509eda31f12a3a0c9f
Jonathan Buch <yellowjon8@hotmail.com>
2015-12-12 07:49:20 +0000

    hw6 silliness

--- CS210/hw6/uml2uni.java
+++ CS210/hw6/uml2uni.java
@@ -1,105 +1,116 @@
 // Jonathan Buch, CS 210 HW#6, Java Assignment
 
 import java.io.*;
-import java.nio.*;
+import java.lang.reflect.Array;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.lang.String;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class uml2uni {
-        public static void main(String[]args) {
-                tokenizer tokens = new tokenizer();
-                tokenizer.read(Paths.get("test.uml"));
-                Path output = Paths.get("uml2uni.icn");
-                tokenizer.outputFile(output);
+    public static void main(String[]args) {
+        tokenizer tokens = new tokenizer();
+
+        String infile;
+        if (args.length > 0) {
+            infile = args[0];
+            tokens.read(Paths.get(infile));
         }
+        else {
+            tokens.read(Paths.get("test.uml"));
+        }
+
+        Path output = Paths.get("uml2uni.icn");
+        tokens.output(output);
+    }
 }
 
 class tokenizer {
-// Hashtable to store the classes
-        Hashtable classH = new Hashtable();
+    // Hashtable to store the classes
+    Hashtable classH = new Hashtable();
 
-// Classes - Hash Table
-        public Hashtable makeclass(String classname) {
-                Hashtable classblock = new Hashtable();
-                classH.put(classname, classblock);
-                classblock.put("methods", new Hashtable());
-                classblock.put("args", new ArrayList());
-                return classblock;
+    // Classes - Hash Table
+    public Hashtable makeclass(String classname) {
+        Hashtable classblock = new Hashtable();
+        classH.put(classname, classblock);
+        classblock.put("methodlist", new Hashtable());
+        classblock.put("args", new ArrayList());
+        classH.put(classname, classblock);
+        return classblock;
+    }
+
+    // Reading a File
+    public void read(Path filename) {
+        File file = new File(filename.toUri());
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(file);
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+            fis.close();
+            String str = new String(data, "UTF-8");
+            parse(str);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-// Reading a File
-        public void read(Path filename) {
-                Charset charset = Charset.forName("US-ASCII");
-                try (BufferedReader reader = Files.newBufferedReader(filename, charset)) {
-                        String line = null;
-                        while((line = reader.readLine()) != null) {
-                                System.out.println(line);
-                        }
-                } catch (IOException x) {
-                        System.err.format("IOException: %s%n", x);
-                }
+    // Writing to a File
+    public void writeF(String line, Path filename) {
+        Charset charset = Charset.forName("US-ASCII");
+        try (BufferedWriter writer = Files.newBufferedWriter(filename, charset)) {
+            writer.write(line, 0, line.length());
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
         }
+    }
 
-// Writing to a File
-        pubilc void writeF(String line, Path filename) {
-                Charset charset = Charset.forName("US-ASCII");
-                try (BufferedWriter writer = Files.newBufferedWriter(filename, charset)) {
-                        writer.write(line, 0, line.length());
-                } catch (IOException x) {
-                System.err.format("IOException: %s%n", x);
+    public void output(Path filename) {
+        String file = "";
+        Enumeration itr =  classH.keys();
+        while (itr.hasMoreElements()) {
+            String className = (String) itr.nextElement();
+            Hashtable classBlock = (Hashtable) classH.get(className);
+            file += String.format("class %s()\n", className);
+            Hashtable methodBlock = (Hashtable) classBlock.get("methodlist");
+            Enumeration methoditr = methodBlock.keys();
+            while (methoditr.hasMoreElements()) {
+                String methodName = (String) methoditr.nextElement();
+                file += String.format("    %s ()\n", methodName);
+            }
+            file += String.format("end\n\n");
         }
+        System.out.print(file);
+        writeF(file, filename);
+    }
 
-// Create Unicon Code
-        public void outputFile(Path filename) {
-                Iterator<String> class = classH.keys();
-                while(class.hasNext()) {
-                        String classname = class.next();
-                        Hashtable classblock = (Hashtable) class.get(classname);
-                        writef(String.format("class %s(%s)%n", classname, concat(", ", (List<String>) classblock.get("arguments"))), file);
-                        Hashtable methodsList = (Hashtable) classblock.get("Cmethods");
-                        Iterator<String> methods = methodlist.keys();
-                        while(methods.next()) {
-                                String methodname = (String) methods.next();
-                                List<String> methodargs = (List<String>) methodlist.get(methodname);
-                                writef(String.printf("   method %s(%s)%n   end%n"
-                                        methodname, concat(", ", methodargs)), filename);
-                        }
-                        writef("end\n", filename);
-                }
+    public void parse(String file) {
+        Pattern classPattern = Pattern.compile("class\\s*([\"\\s\\w]*?)\\s*\\{([^\\{]*)\\}", Pattern.DOTALL | Pattern.MULTILINE);
+        Matcher classMatcher = classPattern.matcher(file);
+
+        while( classMatcher.find() ) {
+            String className = classMatcher.group(1);
+            String classContents = classMatcher.group(2);
+
+            Hashtable classBlock = makeclass(className.replace("\"", "").replace(" ", "_").trim());
+            Hashtable methods = (Hashtable) classBlock.get("methodlist");
+
+            Pattern methodPattern = Pattern.compile("[-+]* *([\\w]*)\\((.*)\\)");
+            Matcher methodMatcher = methodPattern.matcher(classContents);
+
+            while( methodMatcher.find() ) {
+                String methodName = methodMatcher.group(1).trim();
+                String methodArgs = methodMatcher.group(2).trim();
+                methods.put(methodName, methodArgs);
+            }
+
         }
+    }
 
-// Arguments - Strings
-        public List<String> tokenargs(String args) {
-                List<String> arglist = new ArrayList<>();
-                String[] newargs = args.split(",");
-                return arglist;
-
-// Regular Expressions - Hashtable
-        Hashtable regexH = new Hashtable();
-
-
-// Tokenizer
-        public tokenizer() {
-                regexH.put("class", Pattern.compile("class "[a-zA-Z0-9_]+));
-                regexH.put("Cargs", Pattern.compile([a-zA-Z0-9_]+" : "+[a-zA-Z0-9_]+);
-                regexH.put("Cmethods", Pattern.compile("class "[a-zA-Z0-9_]+));
-
-// Line Tokenizer
-        public void linetokens(String line) {
-                String regex, classname, methodname, arg, args;
-                Iterator regexlist = regexh.keys();
-                while(regexlist.hasNext()) {
-                        regex = (String) regexlist.next();
-                        pattern = re
-
-
-// Concatination of a list
-        public String concat(String delimiter, List<String> strings) {
-                Iterator<String> combine = strings.iterator();
-                StringBuilder newstring = new StringBuilder();
-                while(combine.hasNext()) {
-                        newstring.append(combine.next()).append(combine.hasNext() ? delimiter : "");
-                }
-                return newstring.toString();
-        }
 }
