c2cf81f5c4798f579abdb8b53d30fc80ac478221
Jonathan Buch <yellowjon8@hotmail.com>
2016-04-01 18:09:11 +0000

    Changes to hw3.c for CS240

--- CS240/hw3/hw3.c
+++ CS240/hw3/hw3.c
@@ -1,8 +1,8 @@
 /*	Jonathan Buch
-	February 18th, 2016
+	March 30th, 2016
 	CS240
 	Assignment #3
-	Assign2.c
+	hw3.c
 */
 
 
@@ -13,16 +13,16 @@
 #include<sys/types.h>
 
 int makearg( char *s, char*** args, char* delim );
-int myexec(char* cmd, char* output);
-char* rmvwhtspc(char* str);
-char* replace(const char* str, const char* search, const char* replacement);
-int envcommand(char* str);
+int execute( char* cmd, char* output );
+char* rmvwhtspc( char* str );
+char* replace( const char* str, const char* search, const char* replacement );
+int envcmd( char* str );
 
 char* ENV_PATH = "/usr/local/bin:/bin:/usr/bin";
 
 int main(){
     int count = 0;
-	char input[1000];		// Need an array to store input
+	char input[1000];
     int argc = 0;
     int i;
     int childpid;
@@ -34,6 +34,7 @@ int main(){
 	char* pipecmd;
 	int dorc = 1;
 	int line = 0;
+	int flag = 1;
 	FILE* fname;
 	
 	while(1) {
@@ -41,158 +42,174 @@ int main(){
     	printf( "?: " );
     	
     	// Check for .mshrc
-    	if (dorc) {
-    		if (access(".mshrc", F_OK) != -1)
-	    		while (file not empty) {
-	    			cmd = line;
+    	/*
+    	if ( dorc ) {
+    		if ( access( ".mshrc", F_OK ) != -1 ){
+    		    fname = fopen( ".mshrc", "r" );
+    		    
+	    		while ( getchar() != EOF ) {
+	    			cmd = fgets( input, 1000, fname );
 	    			line++;
+	    			flag == 0;
 	    		}
-    		else 
+	    		fclose(fname);
+    		}
+    		else {
     			dorc = 0;
-		} else {
+    			flag = 1;
+    		}
+		} else */ if( flag == 1 ){
 	    	// Get input
-	    	fgets( input, 1000, stdin );
+	    	if( fgets ( input, 1000, stdin ));
 		}
     	
     	// Remove new line from string
-	   	for(i = 0; i < 1000; i++){
-		    if(input[i] == '\n'){ 
+	   	for( i = 0; i < 1000; i++ ){
+		    if( input[i] == '\n' ){ 
 	        	input[i] = '\0';
 		    }
 	    }
 	    
         // Add string to history
-        if (histpos < 4000) {
-            history[histpos] = malloc(strlen(input) + 1);
-            strcpy(history[histpos++], input);
+        if ( histpos < 4000 ) {
+            history[histpos] = malloc( strlen( input ) + 1 );
+            strcpy( history[histpos++], input );
         }
         
         // Process input into tokens for command chaining support ';'
-        cmd = strtok(input, ";");
-        while (cmd) {
-			// Remove leading and trailing whitespace
-			cmd = rmvwhtspc(cmd);
+        cmd = strtok( input, ";" );
+        while ( cmd ) {
+			// Remove whitespace
+			cmd = rmvwhtspc( cmd );
 			
-            // check for echo and export commands
-            if (envcommand(cmd)) {
-                cmd = strtok(NULL, ";"); // move to next command
+            // Check for echo and export
+            if ( envcmd( cmd )) {
+                cmd = strtok( NULL, ";" ); // move to next command
                 continue;
             }
 
-            //replace $PATH with our ENV_PATH variable
-            cmd = replace(cmd, "$PATH", ENV_PATH);
+            // Replace $PATH with new ENV_PATH variable
+            cmd = replace( cmd, "$PATH", ENV_PATH );
 			
-			if (strcmp(cmd, "exit") == 0) {
-				printf("Exiting on request.\n");
+			if ( strcmp( cmd, "exit" ) == 0 ) {
+				printf( "Exiting on request.\n" );
 				exit(0);
-			} else if (strcmp(cmd, "history") == 0) {
-                printf("Command History:\n");
-                for (i = 0; i < histpos; i++) {
-                    printf("%5d %s\n", i, history[i]);
+			} else if ( strcmp( cmd, "history" ) == 0 ) {
+                printf( "Command History:\n" );
+                for ( i = 0; i < histpos; i++ ) {
+                    printf( "%5d %s\n", i, history[i] );
                 }
-            } else if (strcmp(cmd, "!!") == 0) {
-                if (history[histpos-2]) {
-                    cmd = strdup(history[histpos-2]); // get the previous command
-                    printf("%s\n", cmd); // display the previous command
-                    // in history replace !! with the command actually used, like bash
-                    history[histpos-1] = malloc(strlen(cmd) + 1);
-                    strcpy(history[histpos-1], cmd);
+            } else if ( strcmp( cmd, "!!" ) == 0 ) {
+                if ( history[histpos-2] ) {
+                    // Get the previous command
+                    cmd = strdup( history[histpos-2] ); 
+                    printf( "%s\n", cmd );
+                    // In history, replace !! with the command
+                    history[histpos-1] = malloc( strlen( cmd ) + 1 );
+                    strcpy( history[histpos-1], cmd );
 					continue;
                 } else {
-                    printf("-msh: !!: history entry not found\n");
+                    printf( "-msh: !!: history entry not found\n" );
                 }
             }
             // !
-            else if (cmd[0] == '!') {
-                memmove(cmd, cmd+1, strlen(cmd)); // strip first char '!'
-                int pos = atoi(cmd); // convert to int
-                if (pos >= 0 && history[pos] != NULL) {
-                    cmd = strdup(history[pos]); // get the command from history
-                    printf("%s\n", cmd); // display the command from history
-                    // replace !# with the command actually used in history, like bash
-                    history[histpos-1] = malloc(strlen(cmd) + 1);
-                    strcpy(history[histpos-1], cmd);
+            else if ( cmd[0] == '!' ) {
+                // Strip first !
+                memmove( cmd, cmd+1, strlen( cmd ));
+                // Convert to int to find the line number
+                int pos = atoi( cmd ); 
+                if ( pos >= 0 && history[pos] != NULL ) {
+                    // Get the command from history
+                    cmd = strdup( history[pos] ); 
+                    printf( "%s\n", cmd ); 
+                    // In history, replace !# with the command
+                    history[histpos-1] = malloc( strlen( cmd ) + 1 );
+                    strcpy( history[histpos-1], cmd );
 					continue;
                 } else {
-                    printf("-msh: !%d: history entry not found\n", pos);
+                    // Bash error report
+                    printf( "-msh: !%d: history entry not found\n", pos );
                 }
-			} else if (strchr(cmd, '|') == cmd) {
-	        	pipecmd = strtok(cmd, "|");
-	        	while (pipecmd) {
-					// Remove leading and trailing whitespace
-					pipecmd = rmvwhtspc(pipecmd); 
+			} else if ( strchr( cmd, '|' ) == cmd ) {
+	        	pipecmd = strtok( cmd, "|" );
+	        	while( pipecmd ) {
+					// Remove whitespace
+					pipecmd = rmvwhtspc( pipecmd ); 
 	        		// Execute first command
-	        		myexec(pipecmd, output);
+	        		execute( pipecmd, output );
 	        		// Advance to next command
-	        		pipecmd = strtok(NULL, "|");
-	        		// Remove leading and trailing whitespace
-	        		pipecmd = rmvwhtspc(pipecmd);
+	        		pipecmd = strtok( NULL, "|" );
+	        		// Remove whitespace
+	        		pipecmd = rmvwhtspc( pipecmd );
 	        		// Concat output of first command to next command
 	        		char newcmd[4000];
-	        		strcpy(newcmd, pipecmd);
-	        		strcat(newcmd, output); 
+	        		strcpy( newcmd, pipecmd );
+	        		strcat( newcmd, output ); 
 	        		// Execute new command
-	        		myexec(newcmd, output);
+	        		execute( newcmd, output );
 					// Print output
-					printf("%s", output);
+					printf( "%s", output );
 	        	}
 	        } else {
 	            // Execute command
-				myexec(cmd, output);
+				execute( cmd, output );
 				// Print output
-				printf("%s", output);
+				printf( "%s", output );
 	        }
 			
 			// Move to next command
-			cmd = strtok(NULL, ";");
+			cmd = strtok( NULL, ";" );
         }
     }
     
 	return 0;
 }
 
+// Removes whitespace from a supplied string
 char* rmvwhtspc(char* str) {
     char *end;
-
-    while (isspace(*str)) str++; // remove leading spaces
-    if (*str == 0)  // make sure str is not empty now
+    // Remove beginning whitespace
+    while ( isspace( *str )) str++; 
+    if ( *str == 0 )  
         return str;
-    end = str + strlen(str) - 1; // adjust size
-    while (end > str && isspace(*end)) // remove trailing spaces
+    end = str + strlen( str ) - 1; 
+    // Remove trailing spaces
+    while( end > str && isspace( *end ))
         end--;
-    *(end+1) = 0; // put a new null terminator at the end
+    // Place a null at the end of the string
+    *( end+1 ) = 0;
 
     return str;
 }
 
-int myexec(char* cmd, char* output){
+int execute( char* cmd, char* output ){
     char buffer[4000];
-    FILE *fp;
+    FILE* fp;
     int lines = 0;
 
-    // Fork and execute command using popen()
-    if ((fp = popen(cmd, "r")) == NULL) {
-        perror("pipe");
+    // Fork and execute command using popen() instead of execvp
+    if ( ( fp = popen( cmd, "r" )) == NULL ) {
+        perror( "pipe" );
     }
     
-    strcpy(output, "");
+    strcpy( output, "" );
     
     // Read the output of popen()
-    while (fgets(buffer, 4000, fp) != NULL) {
-    	strcat(output, buffer);
+    while ( fgets( buffer, 4000, fp ) != NULL ) {
+    	strcat( output, buffer );
     }
-    pclose(fp);
+    pclose( fp );
     return 0;
 }
 
 int makearg( char *s, char ***args, char* delim ){
 	int i;
 	char* p;
-	int count = 0;				// Start a count
+	int count = 0;				    // Start a count
 	static char **nlist = NULL;		// nlist - newlist/array
-	char *tok = strtok( s, delim );		// Tokenizing Function
+	char* tok = strtok( s, delim );	// Tokenizing Function
 
-	while( tok ){				// While there are still tokens
+	while( tok ){			    	// While there are still tokens
 		count++;
 		nlist = ( char** ) realloc( nlist, sizeof( char* ) * count ); 
 
@@ -201,84 +218,84 @@ int makearg( char *s, char ***args, char* delim ){
 			return -1;
 		}
 		
-		nlist[count - 1] = tok;		// Sets tokenized string to a pointer
+		nlist[count - 1] = tok;		    // Sets tokenized string to a pointer
 				
 		tok = strtok( NULL, delim );	// Increment to next token, if there
-	}					// is one
+	}					                // is one
 
-	
+	// Set original pointer passed to the
+	// list of parsed strings that has 
+	// been created
 
-	*args = nlist;				// Set original pointer passed to the
-						// list of parsed strings that has 
-						// been created
+	*args = nlist;				
 
-	return count;				// Return number of parsed arguements
+    // Return number of parsed arguements
+	return count;				
 }
 
 
-/*
- * replace takes an original string, a search string, and a replacement string.
- * Inside tht replaces all instances of the search string with the replacement
- * string and then returns a pointer to the newly created string.
- */
-char *replace(const char *str, const char *search, const char *replacement) {
-    char *ret, *r;
+// Replace(char, char, char) Takes three strings which when completed,
+// successfully substitutes a string you want to replace inside of a another
+// string with a with a string of your choice
+
+char* replace( const char *str, const char *search, const char *sub ) {
+    char *newstr, *r;
     const char *p, *q;
     size_t count;
 
-    // search for instances of string
-    for (count = 0, p = str; (p = strstr(p, search)); p += strlen(search))
+    // Find the string
+    for ( count = 0, p = str; ( p = strstr( p, search )); p += strlen( search ))
         count++;
-    // return a new string sans the search
-    ret = malloc(count * (strlen(replacement) - strlen(search)) + strlen(str) + 1);
-    if (!ret) // if malloc failed return null string
-        return NULL;
-    // replace all instances with replacement string
-    for (r = ret, p = str; (q = strstr(p, search)); p = q + strlen(search)) {
+    // Return the string without the found string
+        newstr = malloc( count * ( strlen( sub ) - strlen( search )) + strlen( str ) + 1);
+    // Replace the instance with the new string
+    for ( r = newstr, p = str; ( q = strstr( p, search )); p = q + strlen( search )) {
         count = q - p;
-        memcpy(r, p, count);
+        memcpy( r, p, count );
         r += count;
-        strcpy(r, replacement);
-        r += strlen(replacement);
+        strcpy( r, sub );
+        r += strlen( sub );
     }
 
-    strcpy(r, p);
-    return ret;
+    strcpy( r, p );
+    return newstr;
 }
 
 
 /*
- * envcommand splits a string into tokens and then tests and handles various
- * enviornment commands such as export and echo. Returns 1 when the function
- * handles a command, else 0.
+ * envcmd splits a string and checks for the environment commands echo
+ * and export
  */
-int envcommand(char *str) {
+int envcmd( char* str ) {
     int status = 0;
-    char *first, *rest, *context, *context2, *var, *assign;
-    char *mystr = (char*) calloc(strlen(str) + 1, sizeof(char));
-    strncpy(mystr, str, strlen(str)); // make a copy of string, strtok is destructive
+    char* first, *rest, *nstr, *mstr, *var, *assign;
+    char* str1 = ( char* ) calloc( strlen( str ) + 1, sizeof( char ));
+    // Make a copy of the string
+    strncpy( str1, str, strlen( str )); 
 
-    first = strtok_r(mystr, " ", &context); // call thread safe version of strtok
-    rest = context;
+    first = strtok_r( str1, " ", &nstr );
+    rest = nstr;
 
-    if (strcmp(first, "export") == 0) { // is first token the export command?
+    // When echo is found
+    if ( strcmp( first, "echo" ) == 0 ) { 
         status = 1;
-        var = strtok_r(rest, "=", &context2);
-        assign = context2;
-        if (strcmp(var, "PATH") == 0) {
-          assign = replace(assign, "$PATH", ENV_PATH);
+        if ( strcmp( rest, "$PATH" ) == 0 ) {
+            printf( "%s\n", ENV_PATH );
+        } else {
+            printf( "%s\n", rest );
+        }
+    }
+    // If export was found
+    else if ( strcmp( first, "export" ) == 0 ) {
+        status = 1;
+        var = strtok_r( rest, "=", &mstr );
+        assign = mstr;
+        if ( strcmp( var, "PATH" ) == 0 ) {
+          assign = replace( assign, "$PATH", ENV_PATH );
           ENV_PATH = assign;
         }
     }
-    else if (strcmp(first, "echo") == 0) { // else is first token the echo command?
-        status = 1;
-        if (strcmp(rest, "$PATH") == 0) {
-            printf("%s\n", ENV_PATH);
-        } else {
-            printf("%s\n", rest);
-        }
-    }
 
-    free(mystr);
+    free( str1 );
     return status;
 }
